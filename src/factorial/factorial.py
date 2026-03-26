3a4037f6bff6e62ca819e06badbaa9f29b6f3248
Joaquin Poggio <joaquinpoggio04@gmail.com>
2026-03-26 11:59:53 -0300

    ingreso manual y rangos en factorial

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -20,9 +20,28 @@ def factorial(num):
             num -= 1
         return fact 
 
-if len(sys.argv) == 0:
-   print("Debe informar un número!")
-   sys.exit()
-num=int(sys.argv[1])
-print("Factorial ",num,"! es ", factorial(num)) 
+if len(sys.argv) > 1:
+    valor = sys.argv[1]
+else:
+    valor = input("Ingrese número o rango (ej: 4-8, -10, 5-): ")
+
+# Procesar rangos
+if "-" in valor:
+    partes = valor.split("-")
+
+    if partes[0] == "":      
+        desde = 1
+        hasta = int(partes[1])
+    elif partes[1] == "":    
+        desde = int(partes[0])
+        hasta = 60
+    else:                   
+        desde = int(partes[0])
+        hasta = int(partes[1])
+
+    for i in range(desde, hasta + 1):
+        print(f"{i}! = {factorial(i)}")
+else:
+    num = int(valor)
+    print("Factorial ", num, "! es ", factorial(num))
 
